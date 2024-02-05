c0cbfd7d390df76c0d658d9624a8af7b58fbaa4f
BonjourFeng <33033091+BonjourFeng@users.noreply.github.com>
2024-02-05 10:15:58 +0800

    'update'

---
+++ source/output.bat
@@ -0,0 +1,3 @@
+call activate Python39
+python main.py
+exit
---
+++ source/push.bat
@@ -0,0 +1,11 @@
+cd ..
+
+git init
+
+git add -A
+
+git commit -m 'update' 
+
+git push -f https://github.com/BonjourFeng/Script-History.git main
+
+pause
